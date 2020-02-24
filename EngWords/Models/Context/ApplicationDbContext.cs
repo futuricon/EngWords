89a07b216d1dc5b70b75a032b0f95d286ef3a343
Futuricon <keepline@inbox.ru>
2020-02-25 00:45:38 +0500

    Major

---
+++ EngWords/Models/Context/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EngWords.Models.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) {}
+
+        public ApplicationDbContext()
+        {
+            // This is the magic line that made it work for me
+            //SQLitePCL.raw.SetProvider(new SQLitePCL.SQLite3Provider_e_sqlite3());
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=EWSqlite.sqlite");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Entity mappings
+        }
+
+        public DbSet<Word> Words { get; set; }
+    }
+}
---
+++ EngWords/Models/Word.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EngWords.Models
+{
+    public class Word
+    {
+        public int Id { get; set; }
+        public string Eng { get; set; }
+        public string Rus { get; set; }
+        public string Uzb { get; set; }
+        
+    }
+}
