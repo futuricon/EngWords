59b13d18e94b0177c4fdbfd9c069140a833d1fe1
Futuricon <keepline@inbox.ru>
2020-02-24 23:08:53 +0500

    Initial commit

---
+++ EngWords/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using EngWords.Views;
+using Prism.Ioc;
+using Prism.Unity;
+
+namespace EngWords
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            var sell = Container.Resolve<MainView>();
+            return sell;
+        }
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ EngWords/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EngWords.ViewModels
+{
+    public class MainViewModel
+    {
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+    }
+}
