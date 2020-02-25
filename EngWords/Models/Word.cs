75b5bb939103e3257163dc306d5444cf7c3d07be
Futuricon <keepline@inbox.ru>
2020-02-25 12:28:12 +0500

    Word Model Added

--- EngWords/Models/Word.cs
+++ EngWords/Models/Word.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace EngWords.Models
@@ -10,6 +11,6 @@ namespace EngWords.Models
         public string Eng { get; set; }
         public string Rus { get; set; }
         public string Uzb { get; set; }
-        
+       
     }
 }
